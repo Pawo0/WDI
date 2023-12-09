4310dbcc561761b5a032c95bad18b4794918abec
Paweł Czajczyk <czajczykp@gmail.com>
2023-12-09 22:50:16 +0100

    zad22, ale ze zwracaniem

--- zestaw5/zad25.py
+++ zestaw5/zad25.py
@@ -1,20 +1,30 @@
-def max_czynnik(n):
+def czynniki_pierwsze(n):
     i = 2
-    result = 0
+    result = []
     while n != 1:
+        if n % i == 0:
+            result += [i]
         while n % i == 0:
             n //= i
-            result = i
         i += 1
     return result
 
+def zad25(t):
+    n = len(t)
 
-def rek(t, id, l):
-    if max_czynnik(l) == 0:
-        return 0
-
-    m = max_czynnik(l)
-    rek(t, id+m, t[id+m])
+    def rek(i=0,j=0):
+        if i == n-1:
+            return True
+        else:
+            czynniki = czynniki_pierwsze(t[i])
+            for k in range(j, len(czynniki)):
+                if i + czynniki[k] < n and czynniki[k] < t[i]:
+                    return rek(i+czynniki[k], 0) or rek(i, k+1)
+        return False
+    return rek()
 
 
 t = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+t1 = [10, 4, 2, 8, 10, 12, 14, 6, 18, 20, 3]
+
+print(zad25(t1))
