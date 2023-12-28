d58a6fa2583cf023add627e8556389683d2707a3
Paweł Czajczyk <czajczykp@gmail.com>
2023-12-28 20:25:27 +0100

    druga wersja

--- zestaw5/zad6.py
+++ zestaw5/zad6.py
@@ -18,5 +18,22 @@ def zad6(t):
     return result
 
 
+def zad6_v2(T):
+    ile_min = float('inf')
+
+    def rek(i=0,sum_e=0, sum_i=0,ile=0,res=[]):
+        nonlocal ile_min
+        if sum_e != 0 and sum_e == sum_i and ile < ile_min:
+            ile_min = ile
+            print(res)
+            return sum_e
+        if i == len(T):
+            return float('inf')
+        return min(rek(i+1,sum_e,sum_i,ile,res),rek(i+1,sum_e+T[i],sum_i+i,ile+1,res+[i]))
+    return rek()
+
+
+
 t = [1,7,3,5,11,2]
 print(zad6(t))
+print(zad6_v2(t))
